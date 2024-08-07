3718d79bafc0125554424b37971a5a3ab7aef3f5
Lee S Jensen <leesjensen@users.noreply.github.com>
2024-08-07 16:50:29 -0600

    Update orderRouter.js

--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -3,6 +3,7 @@ const config = require('../config.js');
 const { Role, DB } = require('../database/database.js');
 const { authRouter } = require('./authRouter.js');
 const { asyncHandler, StatusCodeError } = require('../endpointHelper.js');
+const metrics = require('../metrics');
 
 const orderRouter = express.Router();
 
@@ -79,6 +80,8 @@ orderRouter.post(
   asyncHandler(async (req, res) => {
     const orderReq = req.body;
     const order = await DB.addDinerOrder(req.user, orderReq);
+    const orderEvent = { count: order.items.length, revenue: order.items.reduce((acc, curr) => acc + curr.price, 0), start: Date.now() };
+
     const r = await fetch(`${config.factory.url}/api/order`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json', authorization: `Bearer ${config.factory.apiKey}` },
@@ -89,7 +92,11 @@ orderRouter.post(
       res.send({ order, jwt: j.jwt, reportUrl: j.reportUrl });
     } else {
       res.status(500).send({ message: 'Failed to fulfill order at factory', reportUrl: j.reportUrl });
+      orderEvent.error = true;
     }
+
+    orderEvent.end = Date.now();
+    metrics.orderEvent(orderEvent);
   })
 );
 
