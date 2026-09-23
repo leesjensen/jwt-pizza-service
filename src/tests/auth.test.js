cf2c1c1e25b7bf1ba5770a3f8236a6874ff1f233
Lee <leesjensen@gmail.com>
2026-09-23 16:35:28 -0600

    add(ci and initial tests)

---
+++ src/tests/auth.test.js
@@ -0,0 +1,32 @@
+const request = require('supertest');
+const app = require('../service');
+
+function randomName() {
+  return Math.random().toString(36).substring(2, 12);
+}
+
+const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
+let testUserAuthToken;
+
+beforeAll(async () => {
+  testUser.email = randomName() + '@test.com';
+  const registerRes = await request(app).post('/api/auth').send(testUser);
+  testUserAuthToken = registerRes.body.token;
+});
+
+test('login', async () => {
+  const loginRes = await request(app).put('/api/auth').send(testUser);
+  expect(loginRes.status).toBe(200);
+  expect(loginRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+
+  const user = { ...testUser, roles: [{ role: 'diner' }] };
+  delete user.password;
+  expect(loginRes.body.user).toMatchObject(user);
+});
+
+test('get menu', async () => {
+  const menuRes = await request(app).get('/api/order/menu').set('Authorization', `Bearer ${testUserAuthToken}`);
+  expect(menuRes.status).toBe(200);
+  const crustyPizza = menuRes.body.find((item) => item.title === 'Crusty');
+  expect(crustyPizza).toBeDefined();
+});
